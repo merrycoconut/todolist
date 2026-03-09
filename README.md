995ea28d24ecadb92b99ddd42b04f92302acf95f
Yaohan Li <lang.liu@Princesss-Mac-Pro.local>
2026-03-09 17:11:30 +0100

    Update the style of usetInput

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # A simple To Do List
+A simple to do list
+
+## Description
+It allow users:
+- write a to do (tbd)
+- change the to do status: active or completed
+- use filter button to show: all, active or completed to do
--- src/UserInput.jsx
+++ src/UserInput.jsx
@@ -1,8 +1,17 @@
 export default function UserInput() {
     // TBD: add input field and button to add new todo item
+    function handleSubmit(e) {
+
+    }
+
     return (
-        <div className="user-input">
-        <p>What is your plan today</p>
+        <div className="user-input-conditioner">
+            <form onSubmit={handleSubmit}>                                
+                <input className="user-input" name="userInput" defaultValue="What is your plan today?  " onChange/>                
+                <button className="submit-button" type="submit">+</button>
+            </form>
+            
         </div>
+        
     )
 }
